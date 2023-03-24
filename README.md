06fdc29ed070e26690117072e99ac63cf836b332
Jack Hsu <jack.hsu@gmail.com>
2023-03-24 15:45:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+```bash
+# start dev server
+npx nx serve demo
+
+# build and serve prod server
+npx nx build demo
+npx nx serve demo --prod
+```
+
---
+++ apps/demo/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.module.css';
+import { Ui } from '@nextorg/ui'
+
+export function Index() {
+  return (
+    <>
+      <p>Test2</p>
+      <Ui />
+    </>
+  );
+}
+
+export default Index;
